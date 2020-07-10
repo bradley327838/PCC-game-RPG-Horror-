80e40f6d108c4ff22c20173b6ff547080481fa43
bradley327838 <bradley327838@gmail.com>
2020-07-10 14:55:44 -0700

    PCC is good

--- game.js
+++ game.js
@@ -35,6 +35,7 @@ var roofList = [
 	]}*/
 ];
 
+var PCCPCC = 3;
 var tileW = 40, tileH = 40;
 var mapW = 20, mapH = 20;
 var currentSecond = 0, frameCount = 0, framesLastSecond = 0, lastFrameTime = 0;
